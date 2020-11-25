b9fe2b03970950398515b973e638de89177745fc
Francisco Pitriqueo <franpitri@gmail.com>
2020-11-25 12:00:57 -0300

    🐛 fix: add missing env variable

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+TAG=$(echo $GITHUB_SHA | head -c7)
+
 TEMPLATE_FILE="$GITHUB_WORKSPACE/deploy/templates/worker-deployment.template.yaml"
 
 sed 's/<TAG>/'$TAG'/g' $TEMPLATE_FILE > $GITHUB_WORKSPACE/deploy/manifests/worker-deployment.yaml
